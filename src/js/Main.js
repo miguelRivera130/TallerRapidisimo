3c2552e08f9863d21460a672e1e1c5ae8d2821a1
Miguel Angel Rivera <angel1301999@gmail.com>
2019-09-12 07:38:20 -0500

    implementacion de las interfaces

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,35 @@
-let pantalla;
-function setup(){
+let pantallaInicial, pantallaFranquicias, pantallaHistorial, pantallaPlatillosD, pantallaPlatillosK;
 
+let pantalla;
+function setup() {
+    pantallaInicial = loadImage('../lib/inicio/fondoInicial.png');
+    pantallaHistorial = loadImage('../lib/inicio/fondoHistorial');
+    pantallaFranquicias = loadImage('../lib/franquicia/fondoInicial.png');
+    pantallaPlatillosD = loadImage('../lib/franquicia/platillosDominos.png');
+
+
+    pantalla = 0;
 }
-function draw(){
-    switch(pantalla){
+function draw() {
+    switch (pantalla) {
+        case 0:
+            Image(pantallaInicial, 0, 0);
+            break;
         case 1:
+            Image(pantallaInicial, 0, 0);
+            break;
+        case 2:
+            Image(pantallaInicial, 0, 0);
+            break;
+        case 3:
+            Image(pantallaFranquicias, 0, 0);
+            break;
+        case 4:
+            Image(pantallaHistorial, 0, 0);
+            break;
+        case 5:
+            Image(pantallaPlatillosD, 0, 0);
             break;
     }
+
+}
